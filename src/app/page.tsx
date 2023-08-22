7d3f810c9a5eba18a0448e6ab5844939f19f66bb
Brian Meek <brian@hntlabs.com>
2023-08-22 11:03:36 -0700

    Add viem sample
    Signed-off-by: Brian Meek <brian@hntlabs.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,39 @@ import { useEffect, useState } from "react";
 import { publicClient } from "./client";
 import { getContract } from "viem";
 import { ERC721ABI } from "./ERC271Abi";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 
-const useViem = () => {
-  const [balance, setBalance] = useState<bigint>();
-  useEffect(() => {
-    async function init() {
-      const contract = getContract({
-        address: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
-        abi: ERC721ABI,
-        publicClient,
-      });
-
-      const result = await contract.read.balanceOf([
-        "0xC17114b14505367799484d908ad5513D33d97F99",
-      ]);
-      setBalance(result);
-      console.log(result);
-    }
-    init();
-  }, []);
-  return { balance };
+type Balance = {
+  result: bigint;
+  contract: string;
 };
 
-export default function Home() {
-  const { balance } = useViem();
+export const getServerSideProps: GetServerSideProps<{
+  balance: Balance;
+}> = async () => {
+  const contract = getContract({
+    address: "0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2",
+    abi: ERC721ABI,
+    publicClient,
+  });
+
+  const result = await contract.read.balanceOf([
+    "0xC17114b14505367799484d908ad5513D33d97F99",
+  ]);
+  console.log(result);
+  return {
+    props: {
+      balance: {
+        result: result,
+        contract: "0xC17114b14505367799484d908ad5513D33d97F99",
+      },
+    },
+  };
+};
+
+export default function Home(
+  balance: InferGetServerSidePropsType<typeof getServerSideProps>
+) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
